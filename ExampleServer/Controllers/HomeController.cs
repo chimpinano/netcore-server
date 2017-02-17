80132ccedaddaee8bae8aafbe9d0e4f31dcc1b80
Adrian Hall <adrian@shellmonger.com>
2017-02-16 21:31:25 -0700

    Added logging

--- ExampleServer/Controllers/HomeController.cs
+++ ExampleServer/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace ExampleServer.Controllers
 {
     public class HomeController : Controller
     {
+        private ILogger logger;
+
+        public HomeController(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger(this.GetType().FullName);
+        }
+
         public IActionResult Index()
         {
+            logger.LogInformation("In Index of the HomeController", null);
             return View();
         }
 
